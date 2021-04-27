deb2602cb53e06f4b254632c217f54bc8d1682b7
Joseph Tallen <jtallen@hawk.iit.edu>
2021-04-26 22:32:49 -0500

    Created a working version with some minor jank eg each roll is sent individually. Removed all redundant dice class implimentations.

--- rollybotjr.py
+++ rollybotjr.py
@@ -1,78 +1,26 @@
 import secrets
-# Removing import in favour of randomdotorg for a more random random
-#import randomdotorg
-# This project was abandoned nearly 6 years ago and will no longer be used
 import discord
 import os
-''' #import random
-#implimenting d4 rolling
-class Roll4Sides:
-    def __init__(self,numofd4):
-        i = 0
-        while i<numofd4:
-            print(secrets.choice([1, 2, 3, 4]))
-            i += 1
-# Attempting to impliment the humble d6
-class Roll6Sides:
-    def __init__(self,numofd6):
-        i = 0
-        while i<numofd6:
-            print(secrets.choice([1, 2, 3, 4, 5, 6]))
-            i += 1
-# Implimenting a d8
-class Roll8Sides:
-    def __init__(self,numofd8):
-        i = 0
-        while i<numofd8:
-            print(secrets.choice([1, 2, 3, 4, 5, 6,7,8]))
-            i += 1
-#implimenting a d10
-class Roll10Sides:
-    def __init__(self,numofd10):
-        i = 0
-        while i<numofd10:
-            print(secrets.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
-            i += 1
-#implimenting a d12
-class Roll12Sides:
-    def __init__(self,numofd12):
-        i = 0
-        while i<numofd12:
-            print(secrets.choice(range(0,13)))
-            i += 1
-#implimenting a d20
-class Roll20Sides:
-    def __init__(self,numofd20):
-        i = 0
-        while i<numofd20:
-            print(secrets.choice(range(0,21)))
-            i += 1
-#implimenting a d100
-class Roll100Sides:
-    def __init__(self,numofd100):
-        i = 0
-        while i<numofd100:
-            print(secrets.choice(range(0,101)))
-            i += 1
-#implimenting a die I might just use this guy for the full implimentation. '''
-class RollAnySides:
-    def __init__(self,numofdice,sides):
-        i = 0
-        self.result_list = []
-        while i < numofdice:
-            self.result_list = self.result_list.append(secrets.choice(range(1,(sides+1))))
-            i += 1
+
+#implimenting a die I might just use this guy for the full implimentation. 
+# class RollAnySides:
+#     def __init__(self,numofdice,sides):
+#         i = 0
+#         self.result_list = []
+#         while i < numofdice:
+#             self.result_list = self.result_list.append(secrets.choice(range(1,(sides+1))))
+#             i += 1
 
 msg = "Greetings fleshy beings. I am Rollybot jr./nType $help for a command list"
 help_msg = "To roll a number of dice of any sides senter $roll _d_. Eg 2d20"
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print('we have logged in as [0.user]'.format(client))
-#channel = client.get_channel(833840635831648296)
-    await client.get_channel("833840635831648296").send(msg)
-       
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening,name='for the $help command'))
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -90,12 +38,14 @@ async def on_message(message):
         m_list = m.split()
         m_map_object = map(int,m_list)
         m_list_of_integers = list(m_map_object)
-        # i = 0 
-        die_roll = RollAnySides(m_list_of_integers[0],m_list_of_integers[1])
-        # while i < m_list_of_integers[0]: #first number is the number of dice
-        #     await message.channel.send(secrets.choice(range(0,(m_list_of_integers[1]+1))))
-        #     i += 1
-        await message.channel.send(die_roll.result_list)
+        m_list_of_results = []
+        i = 0 
+        #die_roll = RollAnySides(m_list_of_integers[0],m_list_of_integers[1])
+        while i < m_list_of_integers[0]: #first number is the number of dice
+            await message.channel.send(secrets.choice(range(1,(m_list_of_integers[1]+1))))
+            i += 1
+       # await message.channel.send(die_roll.result_list)
+        #print(m_list_of_results)
         #await message.channel.send(m_list_of_results)
 
 client.run('TOKEN')
